ead227c2b814060305f5577a121a7419729d3913
ken7253 <ken7253@gmail.com>
2021-11-06 14:45:33 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  rules: {}
+};
