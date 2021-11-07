025249e1de741ed0a30ec106b563c117fdd24bc0
ken7253 <ken7253@gmail.com>
2021-11-07 10:34:03 +0900

    テストケースを先に記述

---
+++ test/modules/Paint.test.ts
@@ -0,0 +1,13 @@
+import Paint from "../../src/modules/Paint";
+
+const paint = new Paint();
+
+describe('🤔Paint.ts', () => {
+  test('paint Method', () => {
+    expect(paint.paint('test message', {
+      color: 'black',
+      background: 'blue',
+      bold: false
+    })).toBe('\u001b[30m\u001b[44mtest message\u001b[0m');
+  });
+});
