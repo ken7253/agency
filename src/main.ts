80d8de56332493230aa7ef8ea78e91af64345745
ken7253 <ken7253@gmail.com>
2021-11-29 22:08:53 +0900

    refactor: Commonize processing to displayMethod.

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,45 @@
-import formatter from './function/formatter';
-import getMessage from './function/getMessage';
+import formatter from './lib/formatter';
+import getMessage from './lib/getMessage';
 
 import type { category, DefineMessage } from './types/DefineMessage';
 
 export default class Speak {
   dictionary: DefineMessage;
   category: category | null;
-  readonly output: Console["log"];
+  readonly output: Console['log'];
+
   constructor(dictionary?: DefineMessage) {
     this.dictionary = dictionary || {};
     this.output = console.log;
     this.category = null;
   }
 
-  log(messageId: string): void {
-    this.category = 'log';
-    const data = this.dictionary.log?.message;
+  private display(cat: category, messageId: string): void {
+    this.category = cat;
+    const data = this.dictionary[cat]?.message;
     if (data) {
       const text = getMessage(data, messageId);
       this.output(formatter(this.category, text[0]));
     }
+  }
+
+  log(messageId: string): void {
+    this.display('log', messageId);
   }
 
   error(messageId: string): void {
-    this.category = 'error';
-    const data = this.dictionary.error?.message;
-    if (data) {
-      const text = getMessage(data, messageId);
-      this.output(formatter(this.category, text[0]));
-    }
+    this.display('error', messageId);
   }
 
   warn(messageId: string): void {
-    this.category = 'warn';
-    const data = this.dictionary.warn?.message;
-    if (data) {
-      const text = getMessage(data, messageId);
-      this.output(formatter(this.category, text[0]));
-    }
+    this.display('warn', messageId);
   }
 
   complete(messageId: string): void {
-    this.category = 'complete';
-    const data = this.dictionary.complete?.message;
-    if (data) {
-      const text = getMessage(data, messageId);
-      this.output(formatter(this.category, text[0]));
-    }
+    this.display('complete', messageId);
   }
 
   running(messageId: string): void {
-    this.category = 'running';
-    const data = this.dictionary.running?.message;
-    if (data) {
-      const text = getMessage(data, messageId);
-      this.output(formatter(this.category, text[0]));
-    }
+    this.display('running', messageId);
   }
 }
